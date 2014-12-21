d7a82e7380ceb50fb3abbbbee2e36d32f378a5e0
NeckShen <u9212005@gmail.com>
2014-12-21 13:44:13 +0800

    cpp code

--- Rcpp_code/test.cpp
+++ Rcpp_code/test.cpp
@@ -4,15 +4,17 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 SEXP num_cpp(NumericVector idx) {
   std::vector<double> y(30);
-  std::vector<int> tmp(idx==1);
-  tmp = tmp / 10;
-  std::partial_sum(tmp.begin(), tmp.end(), y.begin());
-  return(wrap(tmp))
+  //std::vector<int> tmp(as<>(idx==1));
+  //tmp = tmp / 10;
+  //std::partial_sum(tmp.begin(), tmp.end(), y.begin());
+  //return(wrap(tmp));
+  return(wrap(y));
 }
 /*** R
-library(Rcpp)
+# library(Rcpp)
 y = rnorm(30)
 x = rep(c(1:3),each=10)
 x = x[order(y)]
+print(x)
 print(num_cpp(x))
 */
